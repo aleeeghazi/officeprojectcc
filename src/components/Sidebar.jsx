d344bdb0d4373988ca91f87a78153c474eb385bc
ubuntu <ali.ghazi.muzaffar@gmail.com>
2022-08-11 13:12:21 +0000

    last update

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,14 +1,20 @@
+import { Button } from '@material-ui/core';
 import React from 'react'
 import {Navigation} from 'react-minimal-side-navigation';
 import 'react-minimal-side-navigation/lib/ReactMinimalSideNavigation.css';
+import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import { logOut } from '../store/auth/authActions';
 
 
 const Sidebar = () => {
-
+const dispatch =useDispatch()
+  const clickHandler = () => {
+    dispatch(logOut())
+  }
   const navigate = useNavigate()
     return (
-        <div style={{display:'flex', flexDirection:'column',width:'20%', backgroundImage: "linear-gradient(to top, #96fbc4 0%, #f9f586 100%)", height:'100%', justifyContent:'center',position:'fixed', borderRadius:'15px'}}>
+        <div style={{display:'flex', flexDirection:'column',width:'20%', backgroundImage: "linear-gradient(to top, #96fbc4 0%, #f9f586 100%)", height:'100%', justifyContent:'flex-end',position:'fixed', borderRadius:'15px'}}>
             <Navigation
               activeItemId="/management/members"
 
@@ -37,6 +43,8 @@ const Sidebar = () => {
                 
               ]}
             />
+
+          <Button variant="outlined" style={{marginTop:'70%'}} onClick={clickHandler}>Logout</Button>
         </div>
           
         
